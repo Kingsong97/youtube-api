8f02c4bf08403a2d27b011d162c1f1a3c519eb8d
Kingsong97 <161429740+Kingsong97@users.noreply.github.com>
2024-06-10 17:34:55 +0900

    3

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { ImYoutube2 } from "react-icons/im";
+import { headerMenu } from "../../data/header";
+import { searchKeyword } from "../../data/header";
+import { snsLink } from "../../data/header";
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <header id="header" role='banner'>
-            header
+        <header id='header' role='banner'>
+            <h1 className='header__logo'>
+                <a href="/">
+                    <em><ImYoutube2 /></em>
+                    <span>GYM<br /><i>youtube</i></span>
+                </a>
+            </h1>
+            <nav className='header__menu'>
+                <ul className='menu'>
+                    {headerMenu.map((menu, key) => (
+                        <li key={key}>
+                            <Link to={menu.src}>
+                                {menu.icon}<span>{menu.title}</span>
+                            </Link>
+                        </li>
+                    ))}
+                    {/* <li><a href="/today"><span>가슴</span></a></li> */}
+                </ul>
+                <ul className='keyword'>
+                    {searchKeyword.map((word, key) => (
+                        <li key={key}>
+                            <Link to={word.src}>
+                                {word.title}
+                            </Link>
+                        </li>
+                    ))}
+                    {/* <li><a href="/search/KingSong">KingSong</a></li> */}
+                </ul>
+            </nav>
+            <div className='header__sns'>
+                <ul>
+                    {snsLink.map((sns, key) => (
+                        <li key={key}>
+                            <a href={sns.url} target='_blankl' rel='noopener noreferrer' aria-label='{sns.title}'>
+                                <span>{sns.icon}</span>
+                            </a>
+                        </li>
+                    ))}
+                    {/* <li><a href="/">aa</a></li> */}
+                </ul>
+            </div>
         </header>
     )
 }
---
+++ src/data/header.js
@@ -0,0 +1,117 @@
+import { AiFillGithub } from "react-icons/ai";
+import { FaYoutube } from "react-icons/fa";
+import { ImBlogger2 } from "react-icons/im";
+import { FaInstagram } from "react-icons/fa";
+
+import { ImYoutube2 } from "react-icons/im";
+import { CgGym } from "react-icons/cg";
+import { GiGymBag } from "react-icons/gi";
+import { GiLeg } from "react-icons/gi";
+import { GiStrongMan } from "react-icons/gi";
+import { MdOutlineSportsGymnastics } from "react-icons/md";
+import { GiStrong } from "react-icons/gi";
+import { AiFillYoutube } from "react-icons/ai";
+
+export const headerMenu = [
+    {
+        title: "가슴",
+        icon: <CgGym />,
+        src: "/"
+    },
+    {
+        title: "등",
+        icon: <GiStrong />,
+        src: "/"
+    },
+    {
+        title: "하체",
+        icon: <GiLeg />,
+        src: "/"
+    },
+    {
+        title: "어깨",
+        icon: <GiStrongMan />,
+        src: "/"
+    },
+    {
+        title: "코어",
+        icon: <MdOutlineSportsGymnastics />,
+        src: "/"
+    },
+    {
+        title: "추천영양제",
+        icon: <GiGymBag />,
+        src: "/"
+    },
+    {
+        title: "추천유튜버",
+        icon: <AiFillYoutube />,
+        src: "/"
+    }
+]
+
+export const searchKeyword = [
+    {
+        title: "Bench_Press",
+        src: "/search/Bench_Press"
+    },
+    {
+        title: "Deadlift",
+        src: "/search/Deadlift"
+    },
+    {
+        title: "Bench_Press",
+        src: "/search/"
+    },
+    {
+        title: "Squat",
+        src: "/search/Squat"
+    },
+    {
+        title: "Overhead_Press",
+        src: "/search/Overhead_Press"
+    },
+    {
+        title: "Pull_Up",
+        src: "/search/Pull_Up"
+    },
+    {
+        title: "Push_Up",
+        src: "/search/Push_Up"
+    },
+    {
+        title: "Crunch",
+        src: "/search/Crunch"
+    },
+    {
+        title: "Barbell_Curl",
+        src: "/search/Barbell_Curl"
+    },
+    {
+        title: "Dumbbell_Curl",
+        src: "/search/Dumbbell_Curl"
+    }
+]
+
+export const snsLink = [
+    {
+        title: "github",
+        URL: "https://github.com/kingsong97",
+        icon: <AiFillGithub />
+    },
+    {
+        title: "youtube",
+        URL: "https://github.com/kingsong97",
+        icon: <FaYoutube />
+    },
+    {
+        title: "blog",
+        URL: "https://github.com/kingsong97",
+        icon: <ImBlogger2 />
+    },
+    {
+        title: "instagram",
+        URL: "https://github.com/kingsong97",
+        icon: <FaInstagram />
+    },
+]
